069d0c3b518941b0317b9142d0d6172f70793129
Abstrack <Armon_ah05@outlook.com>
2022-07-04 11:02:21 -0400

    added controllers and routes 18.1

--- server.js
+++ server.js
@@ -10,4 +10,12 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍  Connected on localhost:${PORT} 🌍`));
