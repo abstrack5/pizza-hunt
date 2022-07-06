d0304384ac56b8f8e0d395ee8830af0118086265
Abstrack <Armon_ah05@outlook.com>
2022-07-06 18:52:52 -0400

    worked on reply adding/removing 18.3

--- models/Pizza.js
+++ models/Pizza.js
@@ -36,8 +36,13 @@ const PizzaSchema = new Schema(
 );
 
 // get total count of comments and replies on retrieval
+// Here we're using the .reduce() method to tally up the total of every comment with its replies. 
+// In its basic form, .reduce() takes two parameters, an accumulator and a currentValue. 
+// Here, the accumulator is total, and the currentValue is comment. As .reduce() walks through the array, 
+// it passes the accumulating total and the current value of comment into the function, 
+// with the return of the function revising the total for the next iteration through the array.
 PizzaSchema.virtual("commentCount").get(function () {
-  return this.comments.length;
+  return this.comments.reduce((total, comment) => total + comment.replies.length +1, 0);
 });
 
 const Pizza = model("Pizza", PizzaSchema);
